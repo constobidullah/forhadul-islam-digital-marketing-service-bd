f37b0f894d3b29ecfd8b39dce5c9626c3b75764b
unknown <obidullahbinmasud124@gmail.com>
2023-02-08 01:14:50 +0600

    marketing agency section complete

--- src/components/MarketingAgency/MarketingAgency.js
+++ src/components/MarketingAgency/MarketingAgency.js
@@ -3,8 +3,20 @@ import './MarketingAgency.css'
 
 const MarketingAgency = () => {
     return (
-        <div>
-            <h1></h1>
+        <div className='marketing-agency-parents'>
+            <h1>Google & Facebook Certified Digital Marketing Agency.</h1>
+            <p>We are Professionals in digital marketing, web designing, web development, and software development services.</p>
+
+            <div className='marketing-container'>
+                <div>
+                    <iframe className='marketing-video' width="560" height="335" src="https://www.youtube.com/embed/NzNQqVB1U4k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div className='marketing-details'>
+                    <p>DMS is an energetic, results-oriented advanced showcasing Digital Marketing Agency. We convey end-to-end site plans, unique techniques, and versatile improvement. We offer you the most valuable services that assist you in achieving your goals. </p>
+
+                    <p>Our team of digital marketing experts guarantees customer success on every project, from strategy and setup through online promotion, design, and development services. We are a reputed company in Bangladesh that has helped thousands of companies with 100% success.</p>
+                </div>
+            </div>
         </div>
     );
 };
